d48c1a7453d983843e7534b9222ed5080677f74f
dengjun123 <1194898045@qq.com>
2020-06-24 15:18:16 +0800

    first commit

---
+++ HogWarts12/testing/abc_login.py
@@ -0,0 +1,14 @@
+# import yaml
+# def test_login():
+#     print("login")
+#
+# class LoginCases:
+#     def case_a(self):
+#         print("LoginCases ===  case_a")
+#
+#     def test_a(self):
+#         print("LoginCases ===  test_a")
+
+
+# with open("./test.yaml","rb") as f:
+#     print(yaml.safe_load(f))
